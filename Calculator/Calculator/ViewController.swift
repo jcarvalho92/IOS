5a502d8fc0ab3b0ffbba792db2043c7e4440123d
Juliana <julianadecarvalho92@gmail.com>
2020-10-02 17:53:18 -0400

    Added Student Id in the comment header.

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -1,10 +1,11 @@
 //
 //  ViewController.swift
 //  Calculator
-//
+//  Student Id: 30113760
 //  Created by Juliana de Carvalho on 2020-09-30.
 //  Copyright © 2020 Juliana de Carvalho. All rights reserved.
-//
+//  Version 1.0
+//  Build 1
 
 import UIKit
 
@@ -12,7 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
